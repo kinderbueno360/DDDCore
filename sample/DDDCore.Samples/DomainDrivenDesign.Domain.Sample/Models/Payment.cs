3582294ddeb537b823eb36773492cd21514421fb
carlos bueno <carlos.bueno@s-money.fr>
2020-07-27 00:16:01 +0100

    Add class event

--- sample/DDDCore.Samples/DomainDrivenDesign.Domain.Sample/Models/Payment.cs
+++ sample/DDDCore.Samples/DomainDrivenDesign.Domain.Sample/Models/Payment.cs
@@ -10,25 +10,61 @@
     public class Payment : Entity
     {
 
-        public Card Card { get; protected set; }
+        public string CardNumber { get; protected set; }
 
-        public Money Amount { get; protected set; }
+        public string CVV { get; protected set; }
+
+        public string ExpirationDate { get; protected set; }
+
+        public decimal Amount { get; protected set; }
 
         public string BeneficiaryAlias { get; protected set; }
 
-
         public DateTime CreatedOn { get; protected set; }
 
+        public string CardHint => $"{CardNumber.Substring(0, 6)}XXXXXX{CardNumber.Substring(12, CardNumber.Length - 12)}";
 
-        public static Result<Payment> CreateNewCardPayment(Card card, Money amount, string beneficiaryAlias)
+        private Payment(string cardNumber, string cvv, DateTime expirationDate, decimal amount, string beneficiaryAlias) : base()
         {
-            return Result.Success(new Payment(card, amount, beneficiaryAlias));
-        }
 
+             if (cardNumber is null) 
+             {
+                return throw ValidationException("Card number was not valid");
+             }
+             
+            Regex regex = new Regex(@"^4[0-9]{12}(?:[0-9]{3})?$");//Visa Card Regex: just for testing purpose
+            Match match = regex.Match(cardNumber);
 
-        private Payment(Card card, Money amount, string beneficiaryAlias) : base()
-        {
-            this.Card = card;
+            if(!match.Success)
+            {
+                return throw ValidationException("Card number was not valid");
+            }
+            
+            Regex regex = new Regex(@"^[0-9]{3,4}$");
+            match = regex.Match(cvv);
+
+            if(!match.Success)
+            {
+                return throw ValidationException("CVV should be Four or Three digit");
+            }
+
+            Regex regex = new Regex(@"^[0-9]{3,4}$");
+            match = regex.Match(cvv);
+
+            if(!match.Success)
+            {
+                return throw ValidationException("CVV should be Four or Three digit");
+            }
+
+            if (expirationDate < DateTime.Now)
+            {
+                return throw ValidationException("Invalid card expiration date");
+            }
+
+            // Many Validations here and those validations don't belong to Payment
+            this.CardNumber = cardNumber;
+            this.CVV = cvv;
+            this.ExpirationDate = expirationDate;
             this.Amount = amount;
             this.BeneficiaryAlias = beneficiaryAlias;
             this.CreatedOn = DateTime.Now;
