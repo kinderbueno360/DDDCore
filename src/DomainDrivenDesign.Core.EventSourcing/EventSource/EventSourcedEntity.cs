99681c921f8b963d0baee0c24332878977d8ff00
carlos bueno <carlos.bueno@s-money.fr>
2020-07-27 11:35:46 +0100

    Add Project EventSouring and class EventSourcedEntity

---
+++ src/DomainDrivenDesign.Core.EventSourcing/EventSource/EventSourcedEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace DomainDrivenDesign.Core.EventSourcing.EventSource
+{
+    using DomainDrivenDesign.Core.Events;
+    using DomainDrivenDesign.Core.Models;
+
+    public abstract class EventSourcedEntity : Entity
+    {
+        public int Version { get; protected set; }
+
+        public abstract void Apply(Event @event);
+    }
+}
